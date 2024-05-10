fa3f75c10e4eac7dc1fe5a28febd0c2bc161ff45
Mohamed Waleed <mohamed.waleed1023@gmail.com>
2024-05-10 12:35:34 +0300

    Fixed game over condition
    game over includes if there is no more coins for any of the players.
    updating gui when the human plays properly.

--- game_manager.py
+++ game_manager.py
@@ -13,41 +13,49 @@ class GameManager:
         self.main_gui.__del__()
     
     def main(self, x, y):
+        # if no moves available for both at the the same time
+        if self.game_over():
+            return
+
         if self.can_move('b'):
             if not self.valid_move(x, y, 'b'):
                 return
             self.board.update_board(x, y, 'b')
-            self.main_gui.update_GUI_board(self.board.board, [])
+            self.main_gui.update_GUI_board(self.board.board, self.board.get_flip_moves('b'))
             self.board.players[0].coins -= 1
 
-        if self.can_move('w'):
+        while self.can_move('w'):
             x, y = self.computer_player.make_move(copy.deepcopy(self.board))
             self.board.update_board(x, y, 'w')
             self.main_gui.update_GUI_board(self.board.board, self.board.get_flip_moves('b'))
             self.board.players[1].coins -= 1
-            while not self.can_move('b') and self.can_move('w'):
-                x, y = self.computer_player.make_move(copy.deepcopy(self.board))
-                self.board.update_board(x, y, 'w')
-                self.main_gui.update_GUI_board(self.board.board, self.board.get_flip_moves('b'))
-                self.board.players[1].coins -= 1
+            if self.can_move('b') or self.board.players[1].coins == 0:
+                break
 
         self.board.update_score(self.board.players)
         self.main_gui.update_dashboard(self.board.players[0].score, self.board.players[1].score, self.board.players[0].coins, self.board.players[1].coins)
-        
-        if self.can_move('b'):
+
+        if self.game_over():
             return
-        
-        if self.board.players[0].score > self.board.players[1].score:
-            self.main_gui.show_winner("You Won!")
-        elif self.board.players[0].score < self.board.players[1].score:
-            self.main_gui.show_winner("Computer Won!")
-        else:
-            self.main_gui.show_winner("It's a Draw!")
 
     def can_move(self, c):
         return self.board.get_flip_moves(c) != []
 
     def valid_move(self, x, y, c):
         return self.board.valid_move(x, y, c) != []
-    
+
+    def game_over(self):
+        if not self.can_move('b') and not self.can_move('w') \
+            or self.board.players[0].coins == 0 or self.board.players[1].coins == 0:
+
+            if self.board.players[0].score > self.board.players[1].score:
+                self.main_gui.show_winner("You Won!")
+            elif self.board.players[0].score < self.board.players[1].score:
+                self.main_gui.show_winner("Computer Won!")
+            else:
+                self.main_gui.show_winner("It's a Draw!")
+            return True
+        return False
+
+
 gm = GameManager()
